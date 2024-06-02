9cafbf716dffc6135bd5ced0a9a15b2b34f5d4c4
Justine <118758009+AgustinUno@users.noreply.github.com>
2024-06-02 16:14:02 +0800

    SAMPLE SQL

---
+++ DATABASE/admin_data.sql
@@ -0,0 +1,48 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jun 02, 2024 at 10:09 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `admin_info`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_data`
+--
+
+CREATE TABLE `admin_data` (
+  `user_id` varchar(20) NOT NULL,
+  `pass` varchar(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin_data`
+--
+
+INSERT INTO `admin_data` (`user_id`, `pass`) VALUES
+('admin', 'admin'),
+('justine', '123'),
+('raven', '456'),
+('qwer', '123');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
